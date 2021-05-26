7a538b38d90a680f488de886cad44c8566a5f8bf
Igor Chernov <cher.igor@outlook.com>
2021-05-26 23:46:27 +0300

    Генерация имен на основе случайных английских слов
    - Добавил пакет english_words, который содержит несколько тысяч наиболее часто используемых английских слов

--- codelabs_first_app/lib/main.dart
+++ codelabs_first_app/lib/main.dart
@@ -2,6 +2,7 @@
 // Материал - это стандартный язык визуального дизайна для мобильных устройств и Интернета. 
 // Flutter предлагает богатый набор виджетов Material.
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 // Основной метод использует обозначение стрелки ( => ). 
 // Используйте стрелку для однострочных функций или методов.
@@ -12,20 +13,24 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   // Основная задача виджета - предоставить метод build который описывает, 
   // как отображать виджет с точки зрения других виджетов более низкого уровня.
+  // Метода build запускается каждый раз, когда MaterialApp требует рендеринга.
   @override
-  Widget build(BuildContext context) => MaterialApp(
-    title: "Welcome to Flutter",
-    // Виджет Scaffold из библиотеки материалов предоставляет панель приложения по умолчанию, 
-    // заголовок и свойство body, которое содержит дерево виджетов для главного экрана. 
-    // Поддерево виджетов может быть довольно сложным.
-    home: Scaffold(
-      appBar: AppBar(title: const Text("Welcome to Flutter")),
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return MaterialApp(
+      title: "Welcome to Flutter",
+      // Виджет Scaffold из библиотеки материалов предоставляет панель приложения по умолчанию, 
+      // заголовок и свойство body, которое содержит дерево виджетов для главного экрана. 
+      // Поддерево виджетов может быть довольно сложным.
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Welcome to Flutter")),
       // Тело этого примера состоит из виджета Center содержащего дочерний виджет Text . 
       // Виджет « Center выравнивает свое поддерево виджета по центру экрана.
-      body: const Center(
-        child: const Text("Hello World")
+        body: Center(
+          child: Text(wordPair.asPascalCase)
+        ),
       ),
-    ),
-  );
+    );
+  } 
   
 }
