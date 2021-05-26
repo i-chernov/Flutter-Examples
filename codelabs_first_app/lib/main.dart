70ea934028b4a7738db8c0e92c48f8a3be0e3ae0
Igor Chernov <cher.igor@outlook.com>
2021-05-27 01:10:42 +0300

    Бесконечная генерация слов
    - Создал Виджет с отслеживанием состояния
    - Добавил ListView с бесконечной прокруткой

--- codelabs_first_app/lib/main.dart
+++ codelabs_first_app/lib/main.dart
@@ -16,21 +16,53 @@ class MyApp extends StatelessWidget {
   // Метода build запускается каждый раз, когда MaterialApp требует рендеринга.
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
       title: "Welcome to Flutter",
-      // Виджет Scaffold из библиотеки материалов предоставляет панель приложения по умолчанию, 
-      // заголовок и свойство body, которое содержит дерево виджетов для главного экрана. 
-      // Поддерево виджетов может быть довольно сложным.
-      home: Scaffold(
-        appBar: AppBar(title: const Text("Welcome to Flutter")),
-      // Тело этого примера состоит из виджета Center содержащего дочерний виджет Text . 
-      // Виджет « Center выравнивает свое поддерево виджета по центру экрана.
-        body: Center(
-          child: Text(wordPair.asPascalCase)
-        ),
-      ),
+      home: RandomWords()
     );
   } 
+}
+
+// StatefulWidget поддерживают состояние , которое может меняться в течение жизни виджета.
+// Сам объект StatefulWidget является неизменяемым, 
+// но объект State сохраняется в течение всего времени существования виджета.
+class RandomWords extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _RandomWordsState();
+}
+
+// Класс состояния RandomWords
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[]; // Cохранение предлагаемых пар слов.
+  final _biggerFont = const TextStyle(fontSize: 18.0); // Увеличение размера шрифта
+
+  // Этот метод создает ListView который отображает предлагаемую пару слов.
+  // Класс ListView предоставляет свойство itemBuilder , 
+  // которое является анонимной функцией. 
+  // В функцию передаются два параметра - BuildContext и итератор строки i. 
+  // Итератор начинается с 0 и увеличивается каждый раз при вызове функции, 
+  // один раз для каждой предложенной пары слов. 
+  // Эта модель позволяет списку предложений продолжать расти по мере прокрутки пользователя.
+  Widget _buildSuggestions() => ListView.builder(
+    itemBuilder: (BuildContext _context, int i) {
+      if (i.isOdd) return Divider();
+      final int index = i ~/ 2; // Делит на 2 и обрезает плавающею часть
+      if (index >= _suggestions.length) _suggestions.addAll(generateWordPairs().take(10));
+      // Функция _buildSuggestions вызывает _buildRow один раз для каждой пары слов
+      return _buildRow(_suggestions[index]);
+    },
+  );
   
+  Widget _buildRow(WordPair pair) => ListTile(
+    title: Text(pair.asPascalCase, style: _biggerFont),
+  );
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Startup Name Generator")),
+      body: _buildSuggestions()
+    );
+  }
+
 }
